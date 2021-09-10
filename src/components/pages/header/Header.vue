ce42d45b9530111097c5c6f479fe4339445c4d69
wanted6610 <vatocat_nn@mail.ru>
2021-09-10 22:18:36 +0300

    add

--- src/components/pages/header/Header.vue
+++ src/components/pages/header/Header.vue
@@ -12,12 +12,18 @@
                     </div>
                 </div>
                 <div class="header-controls">
-                    <button class="header-controls__back back-controls">
+                    <button class="header-controls__btn header-controls__impaired impaired-controls">
+                        <p class="impaired-controls__text">
+                            Версия для слабовидящих
+                        </p>
+                        <div class="header-controls__icon"></div>
+                    </button>
+                    <button class="header-controls__btn header-controls__back back-controls">
                         <a href="#" class="back-controls__link">
                             <p class="back-controls__text">
                                 Перейти на портал Правительства
                             </p>
-                            <div class="back-controls__arrow"></div>
+                            <div class="header-controls__icon"></div>
                         </a>
                     </button>
                 </div>
@@ -44,6 +50,39 @@
     &-brand{
         display: flex;
         align-items: center;
+    }
+}
+
+.header-controls{
+    display: flex;
+    position: relative;
+    &__btn {
+        width: 166px;
+        display: flex;
+        height: 46px;
+        background-color: #E9F5FD;
+        border-radius: 6px;
+        align-items: center;
+        padding: 9px 10px;
+    }
+    &__btn p {
+        font-size: 12px;
+        line-height: 14px;
+        font-weight: 500;
+        text-align: left;
+    }
+}
+
+.impaired-controls{
+    &__text{
+        width: 87px;
+    }
+}
+
+.back-controls{
+    margin-left: 30px;
+    &__text{
+        width: 111px;
     }
 }
 
